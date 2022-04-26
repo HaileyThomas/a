a631b4244dd5e499b2f4dae420f6d22190e96304
HaileyThomas <haileyraebeauty@gmail.com>
2022-04-26 15:48:52 -0700

    complete welcome page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,8 +3,19 @@ import react from "react";
 function Home() {
   return (
     <section className="home-container">
-      <h1>Welcome!</h1>
-      <p>this will be the welcome/homepage</p>
+      <h1>Hailey Thomas Portfolio</h1>
+      <h2>Junior Full Stack Web Developer</h2>
+      <div className="welcome-text-container">
+        <p>
+          Welcome to my portfolio! I am currently the owner and manager of a
+          small cosmetics brand and eCommerce store who is looking to pursue my
+          dreams in web development. With an eye for design and aesthetics, a
+          passion for seeing ideas become reality and experience in team
+          building and collaboration, I feel like I would make a unique fit in
+          your development team!
+        </p>
+      </div>
+      <div className="welcome-image-container"></div>
     </section>
   );
 }
