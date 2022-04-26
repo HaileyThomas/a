c99e6acc4a99d7e74923ee3f3ee5c203f47f7d5a
HaileyThomas <haileyraebeauty@gmail.com>
2022-04-26 14:57:23 -0700

    complete footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,52 @@
 import react from "react";
 
+import Icon from "@mdi/react";
+import { mdiGithub } from "@mdi/js";
+import { mdiLinkedin } from "@mdi/js";
+import { mdiFileAccount } from "@mdi/js";
+
 function Footer() {
+  let currentYear = new Date().getFullYear();
+
   return (
     <section className="footer-container">
-      <p>footer will go here</p>
+      <div className="footer-icons">
+        <a
+          href="https://github.com/HaileyThomas"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <Icon path={mdiGithub} className="footer-icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/haileyraethomas"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <Icon path={mdiLinkedin} className="footer-icon" />
+        </a>
+        <a
+          href="http://www.disney.com"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <Icon path={mdiFileAccount} className="footer-icon" />
+        </a>
+      </div>
+      <div className="footer-text">
+        <p>
+          &copy; Hailey Thomas ({currentYear}) created using React.js{" "}
+          <a
+            href="https://github.com/HaileyThomas/portfolio-v3"
+            target="blank"
+            rel="noopener noreferrer"
+            className="footer-link"
+          >
+            click here
+          </a>{" "}
+          to view the source code.
+        </p>
+      </div>
     </section>
   );
 }
