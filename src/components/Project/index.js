7f75274c58588d6e7b278f52a202a5135eba42d8
HaileyThomas <haileyraebeauty@gmail.com>
2022-04-27 17:52:37 -0700

    finish setting up project card

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,7 +5,34 @@ function Project({ project }) {
     <section className="project-container">
       <div className="project-card">
         <h3>{project.name}</h3>
-        <img src={project.image} alt="" />
+        <div className="project-info-container">
+          <div className="project-image-container">
+            <img
+              src={project.image}
+              alt={project.name}
+              className="project-img"
+            />
+          </div>
+          <div className="project-text-container">
+            <p className="project-text">{project.description}</p>
+            <a
+              href={project.github}
+              target="blank"
+              rel="noopener noreferrer"
+              className="project-link"
+            >
+              GitHub
+            </a>
+            <a
+              href={project.link}
+              target="blank"
+              rel="noopener noreferrer"
+              className="project-link"
+            >
+              Deploy
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
